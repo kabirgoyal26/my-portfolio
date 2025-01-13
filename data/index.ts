47845b5970782854d4cfea5395162d71642d80d6
kabirgoyal26 <coodingmy88@gmail.com>
2025-01-13 18:24:07 +0530

    Add files via upload

---
+++ data/index.ts
@@ -0,0 +1,215 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently diving into the world of data structures, weaving logic into code and building the foundations for efficient solutions",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "THAPAR HIVE",
+    des: "Thapar Hive is a dynamic career and internship portal designed specifically for students of Thapar Institute of Engineering and Technology",
+    img: "my1.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "/ui.earth.com",
+  },
+  {
+    id: 2,
+    title: "BST GAME ",
+    des: "Developed an interactive Binary Search Tree BST visualizer game to enhance understanding of tree data structures.",
+    img: "k2.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "/ui.yoom.com",
+  },
+
+ 
+];
+
+export const testimonials = [
+  {
+    quote: 
+      "Business Communication & Value Science-I (UHU011): B+\n" +
+      "Discrete Mathematics (UCT101): A+\n" +
+      "Fundamentals of Computer Science (UCT104): A-\n" +
+      "Fundamentals of Physics (UPH009): B+\n" +
+      "Principles of Electrical Engineering (UEE002): A-\n" +
+      "Probability & Statistics (UCT102): A-",
+    name: "22-23 ODD Semester",
+    title: "Semester 1 Grades",
+  },
+  {
+    quote: 
+      "BUSINESS COMMUNICATION & VALUE SCIENCE-II (UHU012): A-\n" +
+      "DATA STRUCTURES AND ALGORITHMS (UCT203): A-\n" +
+      "ENVIRONMENTAL STUDIES (UEN003): B+\n" +
+      "FUNDAMENTALS OF ECONOMICS (UHU007): B+\n" +
+      "LINEAR ALGEBRA (UCT201): B+\n" +
+      "PRINCIPLES OF ELECTRONICS (UEC002): B+\n" +
+      "STATISTICAL MODELING (UCT202): B+",
+    name: "22-23 EVEN Semester",
+    title: "Semester 2 Grades",
+  },
+  {
+    quote: 
+      "COMPUTATIONAL STATISTICS (UCT304): A-\n" +
+      "COMPUTER ORGANIZATION & ARCHITECTURE (UCT302): B+\n" +
+      "FORMAL LANGUAGE AND AUTOMATA THEORY (UCT301): A- \n" +
+      "OBJECT ORIENTED PROGRAMMING (UCT303): B+\n" +
+      "SOFTWARE ENGINEERING (UCT305): B+",
+    name: "23-24 ODD Semester",
+    title: "Semester 3 Grades",
+  },
+  {
+    quote: 
+      "DATABASE MANAGEMENT SYSTEMS (UCT402): A-\n" +
+      "DESIGN THINKING (UHU024): B- \n" +
+      "INTRODUCTION TO INNOVATION, IP MANAGEMENT & ENTREPRENEURSHIP (UHU023): B+\n" +
+      "MARKETING RESEARCH & MARKETING MANAGEMENT (UHU025): A-\n" +
+      "OPERATING SYSTEMS (UCT401): A-\n" +
+      "OPERATIONS RESEARCH (UMA019): A- \n" +
+      "SOFTWARE DESIGN WITH UML (UCT403): B+",
+    name: "23-24 EVEN Semester",
+    title: "Semester 4 Grades",
+  }
+];
+
+
+
+
+
+
+export const companies = [
+  {
+    id: 1,
+    name: "",
+    img: "",
+    nameImg: "",
+  },
+  {
+    id: 2,
+    name: "",
+    img: "",
+    nameImg: "",
+  },
+  {
+    id: 3,
+    name: "",
+    img: "",
+    nameImg: "/",
+  },
+  {
+    id: 4,
+    name: "",
+    img: "",
+    nameImg: "",
+  },
+  {
+    id: 5,
+    name: "",
+    img: "",
+    nameImg: "",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Technical Department Head - Urja 2024",
+    desc: "Contributed to the team of 7 developers in the development and management of the Urja Website.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Thapar Fitness Club - TIET",
+    desc: "Led ACE 2023, ACE 2024: Managed all aspects of major conferences, boosting attendee engagement.",
+    className: "md:col-span-2",
+    thumbnail: "/exp2.svg",
+  },
+
+];
+
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/kabirgoyal26", // GitHub link
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+    link: "https://leetcode.com/u/kabirgoyal/", // LeetCode link
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/kabir-goyal-5986b125b/", // LinkedIn link
+  },
+];
+
